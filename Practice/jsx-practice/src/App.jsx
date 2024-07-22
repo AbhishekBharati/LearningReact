5f82b579b77bf0abb2555e4b9ccbb1316aef883a
AbhishekBharati <210305105048@paruluniversity.ac.in>
2024-07-22 18:58:41 +0530

    Learned how to render HTML Elements

---
+++ Practice/jsx-practice/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        <li>Do React</li>
+        <li>Do Workout</li>
+        <li>Complete Project</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App;
